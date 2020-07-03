17a36564b8647201502c8ecda94953e555a1a0cb
Armaaand <armand.gentot@gmail.com>
2020-07-04 01:56:22 +0200

    Premier commit + ajout du haut de page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+import Logo from './img/W3D_logo_white.png'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  topPage: {
+    background: "linear-gradient(#220CAD 97%, #FFFFFF)",
+    // backgroundColor: "#220CAD",
+    color: "white",
+    height: 500,
+  },
+  topPageContent: {
+    maxWidth: "80%",
+    marginLeft: "20%",
+    marginTop: 20,
+    textAlign: "left",
+  },
+  signUpBtn: {
+    boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .5)',
+    backgroundColor: "#E8E8F7",
+  },
+  signInBtn: {
+    boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .5)',
+    color: "#5343BF",
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} className={classes.topPage} >
+          <Grid xs={6} container spacing={3} className={classes.topPageContent}>
+            <Grid item xs={12} >
+              <h1>Les développeurs <br /> heureux au travail sont ici</h1>
+            </Grid>
+
+            <Grid item xs={6}>
+              <Typography variant="subtitle2">
+                WeLoveDevs.com est conçu par des développeurs <br />
+                pour aider les développeurs à trouver l'entreprise où ils seront heureux
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <img src={Logo} alt="Logo" height="100" />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="caption">
+                CDI, freelance, alternance ou stage.
+              </Typography>
+            </Grid>
+            <Grid item xs={2} style={{ maxWidth: 150 }}>
+              <Button variant="contained" className={classes.signUpBtn}>
+                S'inscrire
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button variant="outlined" className={classes.signInBtn}>
+                Se connecter
+              </Button>
+            </Grid>
+            <Grid item xs={7}>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <h1>Contenu</h1>
+        </Grid>
+      </Grid>
     </div>
   );
 }
